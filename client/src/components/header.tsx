76a6aa03d2313df3724e550540c058ea1e4f2795
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-21 20:50:58 +0530

    notification badges

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,4 +1,13 @@
 const Header = () => {
+
+  const headerOptions = [
+    { icon: "/help.svg", alt: "help" , badge: false},
+    { icon: "/message.svg", alt: "message" , badge: true},
+    { icon: "/controls.svg", alt: "controls" , badge: false},
+    { icon: "/notifications.svg", alt: "notifications" , badge: true},
+  ];
+
+
   return (
     <header className="h-[48px] flex items-center justify-between">
       <div className="bg-white w-[614px] rounded-[12px] h-full px-[20px] flex flex-row gap-[10px] items-center">
@@ -6,10 +15,12 @@ const Header = () => {
         <input className="text-[16px] font-medium text-[#808281] w-full outline-none" placeholder="Search your course" />
       </div>
       <div className="w-[500px] h-full flex flex-row items-center justify-between">
-        <img src="/help.svg" alt="help" width={24} height={24} />
-        <img src="/message.svg" alt="message" width={24} height={24} />
-        <img src="/controls.svg" alt="controls" width={24} height={24} />
-        <img src="/notifications.svg" alt="notifications" width={24} height={24} />
+        {headerOptions.map((option, index) => (
+          <div className="flex items-center relative p-[2px]" key={index} >
+            <img src={option.icon} alt={option.alt} width={24} height={24} />
+            <img src="/badge.svg" alt="line" width={11} height={11} className={`${option.badge ? 'block' : 'hidden'} absolute top-0 right-0`} />
+          </div>
+        ))}
         <div className="w-[200px] h-full flex flex-row items-center justify-between">
           <img src="/user.svg" alt="user" width={48} height={48} />
           <p className="text-[18px] font-semibold">Adeline H. Dency</p>
