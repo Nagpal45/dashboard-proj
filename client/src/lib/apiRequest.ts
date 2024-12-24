bbf616f82e5bdbdb5a7f185ac099a1c7ee2b948a
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-24 12:48:18 +0530

    rmoved

--- client/src/lib/apiRequest.ts
+++ client/src/lib/apiRequest.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://dashboard-proj-eight.vercel.app/api",
     withCredentials: true
 })
 
