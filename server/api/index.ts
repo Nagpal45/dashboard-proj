6bc2535392296dcd2965eff12ab246a9373b184e
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-24 13:05:49 +0530

    cors

--- server/api/index.ts
+++ server/api/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dashboard-proj-1b9c.vercel.app',
     credentials: true
 }))
 
